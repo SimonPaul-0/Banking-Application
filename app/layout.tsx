75d1c736d22bfae00c062f3b9702853a70a2e64b
SimonPaul-0 <simonpaul055648@gmail.com>
2024-05-15 01:59:00 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter, Libre_Baskerville } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const libreBaskerville = Libre_Baskerville({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-libre-baskerville'
+});
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizons , a modern banking platform for everbody's use",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${libreBaskerville.variable}`}>{children}</body>
+    </html>
+  );
+}
