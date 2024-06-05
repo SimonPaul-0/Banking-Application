1e03f01d25128ee8c6158179f0638d83c434734f
SimonPaul-0 <simonpaul055648@gmail.com>
2024-06-05 20:25:56 +0530

    changes2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Libre_Baskerville } from "next/font/google";
-import "./globals.css";
+import './globals.css';
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const libreBaskerville = Libre_Baskerville({
