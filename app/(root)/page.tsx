8e424d919e480fe564f61d4a079cfd6658ded0d6
SimonPaul-0 <simonpaul055648@gmail.com>
2024-07-14 17:50:18 +0530

    changes-3

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,36 +1,57 @@
 import HeaderBox from '@/components/HeaderBox'
-import { RightSidebar } from '@/components/RightSidebar';
-import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
-import React from 'react'
+import RecentTransactions from '@/components/RecentTransactions';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = () => {
-    const loggedIN={ firstName:'Simon ', lastName:'JSM', email:'contact@jsmastery.pro'};
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
 
-    return (
-        <section className='home'>
-            <div className='home-content'>
-                <header className='home-header'>
-                  <HeaderBox
-                  type="greeting"
-                  title="Welcome "
-                  user={loggedIN?.firstName || 'Guest'}
-                  subtext="Access and manage your accounts and transactions more effectively than ever."
-                  />
+  if(!accounts) return;
+  
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-                  <TotalBalanceBox 
-                  accounts={[]}
-                  totalBanks={1}
-                  totalCurrentBalance={1250.35}
-                  />
-                </header>  
-                RECENT TRANSACTIONS
-            </div>
-            <RightSidebar
-            user={loggedIN}
-            transactions={[]}
-            banks={[{ currentBalance: 123.50}, { currentBalance:500.50}]}/>
-        </section>
-    )
+  const account = await getAccount({ appwriteItemId })
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
+
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
+    </section>
+  )
 }
 
 export default Home
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
-import type { Metadata } from "next";
-import { Inter, Libre_Baskerville } from "next/font/google";
-import './globals.css';
+export const dynamic = 'force-dynamic'
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-const libreBaskerville = Libre_Baskerville({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-libre-baskerville"
-});
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
 
 export const metadata: Metadata = {
   title: "Horizon",
-  description: "Horizon, a modern banking platform",
+  description: "Horizon is a modern banking platform for everyone.",
   icons: {
-    icon: "/icons/logo.svg"
+    icon: '/icons/logo.svg'
   }
 };
 
@@ -24,10 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${libreBaskerville.variable}`}>
-        {children}
-      </body>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
     </html>
   );
 }
-
